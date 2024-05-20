2685507ef32ed69950bb7aa68c4761e6031b50b5
kuber12 <kubershakya123@hotmail.com>
2024-05-20 19:21:10 +0545

    bakend

--- backend/server.js
+++ backend/server.js
@@ -7,10 +7,7 @@ const mongoose = require('mongoose');
 dotenv.config();
 const port = process.env.PORT || 5000;
 
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
+mongoose.connect(process.env.MONGO_URL)
   .then(() => {
     console.log('Connected to MongoDB');
   })
